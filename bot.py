55fde43e5bcd02980a3514d82e52fcb617487182
Alexander Zhou <alexzhou3999@gmail.com>
2020-12-21 17:59:41 -0800

    started work on bot

--- bot.py
+++ bot.py
@@ -9,20 +9,24 @@ time_2 = 0
 
 screenWidth, screenHeight = pyautogui.size()
 width, height = pyautogui.screenshot().size
-# print(screenWidth, screenHeight)
-# print(screenWidth/2, screenHeight/2)
+scWidth, scHeight = int(width * 0.4), int(height * 0.4)
+scLeft, scTop = int(width/2-scWidth/2), int(height/2-scHeight/2)
+
+print('ready!')
 keyboard.wait(';')
 while True:
 
     time_2 = time.time()
     # keyboard.press_and_release('d')
     # time.sleep(1)
-    if time_2 - time_1 > 1:
-        im = pyautogui.screenshot('mine_sc.png', region=((width/2-700), (height/2-600),1400, 1200))
+    if time_2 - time_1 > 0.5:
+        # im = pyautogui.screenshot(region=(scLeft, scTop, scWidth, scHeight))
+        if pyautogui.locateOnScreen('medium.png', region=(scLeft, scTop, scWidth, scHeight), confidence = 0.7) != None:
+            print('found hook!')
         # im = pyautogui.screenshot('mine_sc.png')
         # print(im.size)
-        print('saved screen shot!')
-        break
+        # break
+        time_1 = time.time()
     if keyboard.is_pressed('t') != False:
         print('--ended program--')
         break
